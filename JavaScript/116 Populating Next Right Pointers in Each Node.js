5d604b5b50b1ea88737d4de2c10d81b9f4e1a5e4
iplus26 <iplus26@gmail.com>
2015-07-24 10:32:08 +0800

    Merge

--- JavaScript/116 Populating Next Right Pointers in Each Node.js
+++ JavaScript/116 Populating Next Right Pointers in Each Node.js
@@ -12,28 +12,7 @@
  * @return {void} Do not return anything, modify tree in-place instead.
  */
 var connect = function(root) {
-    // var depth = 0;
-    var nextNode = [];
 
-    var walk = function(node, depth) {
-        if (node === null) {
-            return;
-        }
-
-        ++depth;
-        if (nextNode[depth] !== null) {
-            node.next = nextNode[depth];
-        }
-        nextNode[depth] = node;
-
-        if (node.right !== null) {
-            walk(node.right, depth);
-        }
-
-        if (node.left !== null) {
-            walk(node.left, depth);
-        }
-    };
-
-    walk(root, 0);
+    while (root.left) {
+    }
 };
