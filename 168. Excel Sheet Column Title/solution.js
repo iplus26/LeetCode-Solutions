08d636ddd498fef5f8a35dc38486bcdd93cf37b6
Ivan Jiang <iplus26@gmail.com>
2016-09-14 02:31:21 +0800

    Updated #168: Use String.fromCharCode instead

--- 168. Excel Sheet Column Title/solution.js
+++ 168. Excel Sheet Column Title/solution.js
@@ -5,8 +5,6 @@
 var convertToTitle = function(n) {
     if (n === 0) return '';
     
-    var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
-        m = (n - 1) % 26; // 0 - 25
-    
-    return convertToTitle((n - m - 1) / 26) + '' + alphabet[m];
+    var m = (n - 1) % 26; // 0 - 25
+    return convertToTitle((n - m - 1) / 26) + '' + String.fromCharCode(m + 65);
 };
