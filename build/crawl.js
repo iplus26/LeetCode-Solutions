6b5176204f89b5c0be9ac2ca2e16a6b9f4d1c2ca
Ivan Jiang <iplus26@gmail.com>
2016-09-14 01:58:28 +0800

    LeetCode is providing an API for this!

--- build/crawl.js
+++ build/crawl.js
@@ -9,19 +9,30 @@
 'use strict';
 
 var rp = require('request-promise'),
-  Promise = require('bluebird'),
-  fs = Promise.promisifyAll(require('fs')),
-  cheerio = require('cheerio');
+  Promise = require('bluebird');
+  // fs = Promise.promisifyAll(require('fs')),
+  // cheerio = require('cheerio');
 
 var crawl = function crawl() {
-  return rp.get('https://leetcode.com/problemset/algorithms/')
-    .then(function(html) {
+  return rp.get('https://leetcode.com/api/problems/algorithms/')
+    .then(function(json) {
+      var key = 'stat_status_pairs',
+          log = console.log,
+          toString = Object.prototype.toString;
 
-      var $ = cheerio.load(html);
+      try {
+          var data = JSON.parse(json);
 
-      // console.log('############### ' + ($('#problemList tr').length - 1) + ' ###############');
+          if (data && toString.call(data[key]) === '[object Array]') {
+              log('Length of leetcode problems: ' + data[key].length);
+              return Promise.resolve(data);
+          }
+          else log('Fail, typeof data: ' + typeof data + ' ' + JSON.stringify(data));
+      } catch(e) {
+          log('Parse json data failed. ');
+      }
 
-      return fs.writeFileAsync(__dirname + '/leetcode.html', $('#problemListRow').html());
+      return Promise.reject(new Error('fetch failed'));
     });
 };
 
--- build/main.js
+++ build/main.js
@@ -11,82 +11,59 @@ var Promise = require('bluebird'),
 
 const LEETCODE_ORIGIN = 'https://leetcode.com';
 
-var getLeetCode = function getLeetCode(count) {
+var getLeetCode = function getLeetCode(data) {
+  return new Promise(function (resolve, reject) {
+    if (data === undefined) {
+      reject('data is undefined');
+    } else {
+      resolve(data);
+    }
+  }) 
+    .then(function(data) {
 
-  count = count || 0;
+      var key = 'stat_status_pairs',
+          problems = [];
 
-  return fs.readFileAsync(__dirname + '/leetcode.html', 'utf-8')
-    .then(function(html) {
-
-      var $ = cheerio.load(html),
-        $problem = $('table#problemList tr');
-
-      var problems = [];
-
-      $problem.each(function(i, el) {
-
-        if (i === 0) {
-          return;
-        }
-
-        var $pr = $(el).find('td');
-
-        /*
-         * <tr> 
-         *   <td>...</td>
-         *   <td> {index} </td>
-         *   <td>
-         *     <a href="/problems/some-problem/">Some Problem</a>
-         *     <i class="fa fa-lock"> ... </i>
-         *   </td>
-         *   <td> {accept rate} </td>
-         *   <td> ... </td>
-         *   <td> ... </td>
-         *   <td> {difficulty} </td>
-         * </tr>
-         */
-
-        var pr = {
-          index: parseInt($pr.eq(1).text()),
-          url: LEETCODE_ORIGIN + $pr.eq(2).children('a').attr('href'),
-          title: $pr.eq(2).children('a').text(),
-          isLock: !!($pr.eq(2).children('i.fa-lock').length),
-          acRate: $pr.eq(3).text(),
-          diff: $pr.eq(6).text(),
-        };
+      data[key].forEach(function(el, i) {
+          var pr = {
+            index: el.stat['question_id'],
+            url: LEETCODE_ORIGIN + el.stat['question__title_slug'],
+            title: el.stat['question__title'],
+            isLock: el['paid_only'],
+            // acRate: $pr.eq(3).text(),
+            diff: ['Easy', 'Medium', 'Hard'][el.difficulty.level + 1],
+          };
 
         problems[pr.index] = pr;
       });
 
-      console.log(chalk.info('Found') + ' ' + ($problem.length - 1) +
-        ' problems listed (build/leetcode.html) locally. ');
+      console.log(chalk.info('Get') + ' ' + data['num_total'] + ' problems. ');
 
       return {
-        count: $problem.length - 1,
+        count: data['num_total'],
         problems: problems
       };
-    })
-    .catch(function(e) {
-      // if file doesn't exist
-      console.log(chalk.warning('Not found locally LeetCode OJ list! '));
-
-      console.log(chalk.info('Fetching') + ' the latest data from Internet... ');
-      return crawlAsync().then(function() {
-        return getLeetCode();
-      });
     });
 
 };
 
 console.log(chalk.info('Welcome') + ' Building your version of LeetCode OJ solutions... ');
 
-getLeetCode()
-  .then(function(data) {
-    // Walk through the local file list
-    return walkAsync(data).then(function() {
-      return writeAsync(data);
-    });
-  })
-  .then(function() {
-    console.log(chalk.info('Use `git diff` to check out the updates. '));
-  });
+crawlAsync()
+    .then(function(data) {
+        console.log(chalk.info('Success') + ' Fetched LeetCode data! ');
+        return getLeetCode(data);
+    })
+    .then(function(data) {
+        console.log(chalk.info('Success') + ' Format raw data! ');
+        return walkAsync(data).then(function() {
+            return writeAsync(data);
+        });
+    })
+    .then(function() {
+        console.log(chalk.info('Use `git diff` to check out the updates. '));
+    })
+
+    .catch(function(e) {
+        console.log('Failed ' + JSON.stringify(e));
+    })
